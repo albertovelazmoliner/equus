a4a2f29b4f90aa0d4aa4b779ba09cb1dfbf58713
Alberto Vélaz <albertovelaz.dev@gmail.com>
2021-11-26 10:12:10 +0100

    Create dbData.js

---
+++ dbData.js
@@ -0,0 +1,23 @@
+const { Client } = require('pg');
+
+async function readDataFromDB() {
+    let result = [];
+    try {
+        const client = new Client({
+            user: 'dbuser',
+            host: 'database.server.com',
+            database: 'mydb',
+            password: 'secretpassword',
+            port: 3211,
+        });
+        await client.connect();
+        const res = await client.query('SELECT first_name, email, date_of_birth FROM users');
+        await client.end();
+        result = res.rows;
+    } catch (err) {
+        console.log(err);
+    }
+    return result;
+}
+
+module.exports.readDataFromDB = readDataFromDB;
