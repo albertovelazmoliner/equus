19f8576728843104f9f45e78761b79d5d1d3afcd
Alberto Vélaz <albertovelaz.dev@gmail.com>
2021-11-26 09:44:59 +0100

    Create birthdayJob.js

---
+++ birthdayJob.js
@@ -0,0 +1,50 @@
+
+const CronJob = require('cron').CronJob;
+const nodemailer = require('nodemailer');
+const dataReader = require('./data');
+const dataParser = require('./parserData')
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
+
+function createOptions(data) {
+    const mailOptions = {
+        from: process.env.EMAIL_ADDRESS,
+        to: data.email,
+        subject: 'Happy birthday!',
+        text: `Happy birthday, dear ${data.first_name}!`
+    };
+    return mailOptions;
+}
+
+async function sendMail() {
+    const data = await dataReader.readData();
+    const selectedTargets = dataParser.getSelectedTargets(data);
+    for (let i = 0; i < selectedTargets.length; i++) {
+        const mailOptions = createOptions(selectedTargets[i]);
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(
+                    `Email sent:\nresponse: ${info.response}\naccepted: ${info.accepted}\nrejected: ${info.rejected}` );
+            }
+        });
+    }
+    
+}
+async function start(){
+    console.log('job started');
+    const job = new CronJob('0 */1 * * * *', async function() {
+        await sendMail();
+    }, null, true, 'America/Los_Angeles');
+    job.start();
+}
+
+module.exports.start = start;
