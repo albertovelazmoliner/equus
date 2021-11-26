b59ee8c940f075fad9c9b39ca2c1b4171d3567c0
Alberto Vélaz <albertovelaz.dev@gmail.com>
2021-11-26 09:44:35 +0100

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 8
+    },
+    "rules": {
+        "no-console": 0
+    },
+}
+  
+  
