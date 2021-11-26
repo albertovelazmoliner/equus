cf75d9610df3ab1d799f8d7075b398510cdd7335
Alberto Vélaz <albertovelaz.dev@gmail.com>
2021-11-26 10:23:02 +0100

    Update dbData.js

--- dbData.js
+++ dbData.js
@@ -20,4 +20,25 @@ async function readDataFromDB() {
     return result;
 }
 
+async function readSMSDataFromDB() {
+    let result = [];
+    try {
+        const client = new Client({
+            user: 'dbuser',
+            host: 'database.server.com',
+            database: 'mydb',
+            password: 'secretpassword',
+            port: 3211,
+        });
+        await client.connect();
+        const res = await client.query('SELECT first_name, phone, date_of_birth FROM users');
+        await client.end();
+        result = res.rows;
+    } catch (err) {
+        console.log(err);
+    }
+    return result;
+}
+
 module.exports.readDataFromDB = readDataFromDB;
+module.exports.readSMSDataFromDB = readSMSDataFromDB;
